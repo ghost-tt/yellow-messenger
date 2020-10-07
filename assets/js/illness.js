e76b06f9958c062eca3d71e7e0fec452afe1e150
Aporve <aporvep@gmail.com>
2020-10-07 14:28:49 +0530

    Ideal timeout URL redirect to same Page

--- assets/js/illness.js
+++ assets/js/illness.js
@@ -133,7 +133,7 @@ function resetTimer() {
 
 function timerIncrement() {
     currSeconds = currSeconds + 1;
-    if (currSeconds == 10) {
+    if (currSeconds == 1800) {
         window.top.location = 'http://www.philamlife.com'
     }
 }
