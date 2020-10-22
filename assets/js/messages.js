f7482d5cadc8df0676cd259711bf6df24a43ac84
aiswaryac895 <aiswarya.c@10xds.com>
2020-10-22 13:42:20 +0530

    progress tracking dropdown

---
+++ assets/js/messages.js
@@ -0,0 +1,29 @@
+var progress_msges = [
+    { 'id': 1, 'msg': "You have completely filled out the form for Accident Claims." },
+    { 'id': 2, 'msg': "You have completely filled out the form for Illness Claims." },
+    { 'id': 3, 'msg': "You have completely filled out the form for Death Claims." },
+    { 'id': 4, 'msg': "You have submitted all the required documents." },
+    { 'id': 5, 'msg': "You have chosen Bank Transfer as a preferred payout method." },
+    { 'id': 6, 'msg': "You have chosen Pick Up Anywhere as a preferred payout method." },
+    { 'id': 7, 'msg': "We have received and reviewed your application." },
+    { 'id': 8, 'msg': "We are requesting additional claims documents." },
+    { 'id': 9, 'msg': "You have submitted all additional documents." },
+    { 'id': 10, 'msg': "We have denied your claim request." },
+    { 'id': 11, 'msg': "We have approved your claim request." },
+    { 'id': 12, 'msg': "Your payout is ready for pick up at any BPI branch." }
+]
+    // "form-complete-accident": "You have completely filled out the form for Accident Claims.",
+    // "form-complete-illness": "You have completely filled out the form for Illness Claims.",
+    // "form-complete-death": "You have completely filled out the form for Death Claims.",
+    // "docs-submitted": "You have submitted all the required documents.",
+    // "bank-tranfer-method": "You have chosen Bank Transfer as a preferred payout method.",
+    // "payout-method": "You have chosen Pick Up Anywhere as a preferred payout method.",
+    // "recieved-appl": "We have received and reviewed your application.",
+    // "additional-docs-required": "We are requesting additional claims documents.",
+    // "additional-docs-submitted": "You have submitted all additional documents.",
+    // "claim-denied": "We have denied your claim request.",
+    // "claim-approved": "We have approved your claim request.",
+    // "pick-up-ready": "Your payout is ready for pick up at any BPI branch."
+
+
+
